b9361c210f1a945eb6478e735650b538d53b3899
Oleh Zarichnyi <olehrichi@gmail.com>
2023-03-09 00:39:12 +0200

    corected the variables

--- homework01/script.js
+++ homework01/script.js
@@ -1,17 +1,41 @@
-// console.log("It's working")
+const samsung = 15.678;
+const apple = 123.965;
+const xiaomi = 90.2345;
+const cash = 500;
 
-// number
-// boolean
-// string
-// null
-// undefined
+const maxPrice = Math.max(samsung,apple,xiaomi);
+console.log ("Максимальне число", maxPrice);
 
-// let age = 10;
-// let isOld = false;
-// let name = "Tomas";
+const minPrice = Math.min(samsung,apple,xiaomi)
+console.log ("Мінімальне число", minPrice);
 
-// console.log(typeof name);
+const price = samsung + apple + xiaomi;
+console.log("Вартість", price);
 
-const name = prompt("Enter your emal")
+const samsungFloor = Math.floor(samsung);
+const appleFloor = Math.floor(apple);
+const xiaomiFloor = Math.floor(xiaomi);
 
-console.log(name)
+const priceFloor = samsungFloor + appleFloor + xiaomiFloor;
+console.log("Округлення", priceFloor);
+
+const hundredsPrice = Math.floor(priceFloor/100)*100;
+console.log("Округлення до сотень", hundredsPrice);
+
+const booleanPrice = priceFloor / 2 ? true : false;
+console.log("Булеве значення:", booleanPrice);
+
+const sum = cash - price;
+console.log("Сума решти:", sum);
+
+const averageValue = price / 3;
+console.log("Середнє значення:", averageValue.toFixed(2))
+
+const discount = Math.random(samsung + apple + xiaomi)*100;
+console.log("Знижка", discount.toFixed(2));
+
+const sumPay = price - discount;
+console.log("Сума до оплати", sumPay.toFixed(2));
+
+const profit = (price / 2) - discount;
+console.log("Прибуток", profit.toFixed(2));
