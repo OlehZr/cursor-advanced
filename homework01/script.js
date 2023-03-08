7ceb58d71b1b3e4c676122085bc9da4865127bf7
Oleh Zarichnyi <olehrichi@gmail.com>
2023-03-08 21:58:32 +0200

    first commit

---
+++ homework01/script.js
@@ -0,0 +1,17 @@
+// console.log("It's working")
+
+// number
+// boolean
+// string
+// null
+// undefined
+
+// let age = 10;
+// let isOld = false;
+// let name = "Tomas";
+
+// console.log(typeof name);
+
+const name = prompt("Enter your emal")
+
+console.log(name)
