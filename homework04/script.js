58e78a5ffbcb133f25cb6009c5a755a23e2b7738
Oleh Zarichnyi <olehrichi@gmail.com>
2023-03-30 11:10:44 +0300

    changed the function for the array

--- homework04/script.js
+++ homework04/script.js
@@ -1,28 +1,35 @@
-const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const students = ["Олександр", "Ігор", "Оленa", "Ірa", "Олексій", "Світланa"];
 const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
 const marks = [4, 5, 5, 3, 4, 5];
 
+// const studentsPairs = [];
+
+//     studentsPairs.push([students[0], students[2]]);
+//     studentsPairs.push([students[1], students[3]]);
+//     studentsPairs.push([students[4], students[5]]);
+const boys = [];
+const girls = [];
 const studentsPairs = [];
 
-    studentsPairs.push([students[0], students[2]]);
-    studentsPairs.push([students[1], students[3]]);
-    studentsPairs.push([students[4], students[5]]);
-// const boys = [];
-// const girls = [];
+    for (let i = 0; i < students.length; i++) {
+        let name = students[i];
+        if (name.endsWith("a")) {
+        girls.push(name);
+        } else {
+        boys.push(name)
+        }
+    }
 
-//     for (let i = 0; i < students.length; i++) {
-//         let name = students[i];
-//         if (name.endsWith('a')) {
-//         girls.push(name);
-//         } else {
-//         boys.push(name);
-//         }
-//     }
-//     const studentsPairs = [];
-//     for (let i = 0; i < Math.min(boys.length, girls.length); i++) {
-//         studentsPairs.push([boys[i], girls[i]]);
-//     }
-console.log(studentsPairs)
+function getPairs(students){ 
+    for (let i = 0; i < students[i].length; i++) {
+        studentsPairs.push([boys[i], girls[i]]);
+    }
+    return studentsPairs
+}
+
+const pairs = getPairs(students)
+console.log(pairs)
+
 
 const studentsSubject = [];
 
