910c1e3a4f131da9224daa6a762be8166dceb5e5
Oleh Zarichnyi <olehrichi@gmail.com>
2023-05-09 23:03:39 +0300

    added work with promisses

---
+++ homework11/js/script.js
@@ -0,0 +1,16 @@
+function getRandomChinese(length){
+    return new Promise((resolve) => {
+        let result = "";
+        let i = 0;
+        while(i < length){ 
+            i++;       
+            const sign = Date.now().toString().slice(-5);
+            let converted = String.fromCharCode(sign);
+            result += converted;
+            setTimeout(function() { 
+                resolve(result);
+            }, i*50);
+        };
+    });
+};
+getRandomChinese(4).then(function(result) {console.log(result)});
